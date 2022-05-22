2ce0ad62a8ec58b41f16bb563067b25866ce4e8b
unknown <emonhowlader1997@gmail.com>
2022-05-22 09:55:57 +0600

    app first commit

---
+++ src/components/Country.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import style from './country.module.css'
+
+
+
+
+const Country = (props) => { 
+    const {name, flags, population, area, capital} = props.country
+
+    const handleRemoveCountry = (name) => {
+        props.onRemoveCountry(name)
+    }
+
+    return (
+        <article className={style.country}>
+            <div>
+                <img src={flags.png} alt={name.common} className={style.flag} />
+                <h3>Name: {name.common}</h3>
+                <h3>Capital: {capital}</h3>
+                <h3>Population: {population}</h3>
+                <h3>Area: {area}</h3>
+                <button onClick={() => {handleRemoveCountry(name.common)}} className={style.btn}>Remove Country</button>
+            </div>
+        </article>
+    );
+};
+
+export default Country;
